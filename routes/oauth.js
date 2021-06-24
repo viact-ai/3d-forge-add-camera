0ac454a691495a2d031a9287859a4958e0a0e092
John S <johnjayasingh.s@gmail.com>
2021-06-24 13:03:08 +0530

    two legged

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,49 +1,28 @@
 const express = require("express");
-
+const axios = require("axios").default;
 const config = require("../config");
 const { OAuth } = require("./common/oauth");
 
 let router = express.Router();
 
-router.get("/callback/oauth", async (req, res, next) => {
-  const { code } = req.query;
-  const oauth = new OAuth(req.session);
-  try {
-    await oauth.setCode(code);
-    res.redirect("/");
-  } catch (err) {
-    next(err);
-  }
-});
-
-router.get("/oauth/url", (req, res) => {
-  const url =
-    "https://developer.api.autodesk.com" +
-    "/authentication/v1/authorize?response_type=code" +
-    "&client_id=" +
-    config.credentials.client_id +
-    "&redirect_uri=" +
-    config.credentials.callback_url +
-    "&scope=" +
-    config.scopes.internal.join(" ");
-  res.end(url);
-});
-
-router.get("/oauth/signout", (req, res) => {
-  req.session = null;
-  res.redirect("/");
-});
-
 // Endpoint to return a 2-legged access token
 router.get("/oauth/token", async (req, res, next) => {
-  const oauth = new OAuth(req.session);
-  if (!oauth.isAuthorized()) {
-    res.status(401).end();
-    return;
-  }
-
   try {
-    const accessToken = await oauth.getPublicToken();
+    const params = new URLSearchParams();
+    params.append("client_id", config.credentials.client_id);
+    params.append("client_secret", config.credentials.client_secret);
+    params.append("grant_type", "client_credentials");
+    params.append("scope", "data:read");
+    const response = await axios.post(
+      "https://developer.api.autodesk.com/authentication/v1/authenticate",
+      params,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    const accessToken = response.data.access_token;
     res.json(accessToken);
   } catch (err) {
     next(err);
