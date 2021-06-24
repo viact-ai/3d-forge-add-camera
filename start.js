c8335f8d3b98af59a26c19b5232ef070217a382d
John S <johnjayasingh.s@gmail.com>
2021-06-24 12:52:27 +0530

    logo

--- start.js
+++ start.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const cookieSession = require("cookie-session");
 
+require("dotenv").config();
+
 const PORT = process.env.PORT || 3000;
 const config = require("./config");
 if (
