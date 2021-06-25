f76aed4b7b93a4cea672ac351854cb6e08cd8377
John S <johnjayasingh.s@gmail.com>
2021-06-25 10:49:54 +0530

    school

--- public/js/ForgeViewer.js
+++ public/js/ForgeViewer.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   getForgeToken(function (res) {
     // yes, it is signed in...
     const urn =
-      "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6N3F4cHdndzl1eXl5eGQwZGJzeHdycmt2bWtwdGpiYmctZm9yZ2V0ZXN0YnVja2V0L0NvbW11bml0eV9TY2hvb2wub2Jq";
+      "dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6N3F4cHdndzl1eXl5eGQwZGJzeHdycmt2bWtwdGpiYmctZm9yZ2V0ZXN0YnVja2V0L0NvbW11bml0eV9TY2hvb2wuM2Rz";
     // finally:
     launchViewer(urn);
   });
